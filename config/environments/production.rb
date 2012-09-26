12aef97e35bed9d7503512d9863fe208f7da6731
Jon McCartie <jon@mccartie.com>
2012-09-25 22:23:26 -0700

    added default host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Socalbouldering::Application.configure do
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => ENV["HEROKU_HOST"] || "www.socalbouldering.com" }
   ### ActionMailer Config
   # Setup for production - deliveries, no errors raised
   # config.action_mailer.delivery_method        = :sendmail
